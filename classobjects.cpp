a5273795c1e14073b0cdc8b0c601b80e85b4f490
InderKang <150659302+InderKang@users.noreply.github.com>
2026-03-22 12:48:14 +0530

    Refactor rectangle class to calculate triangle area

--- classobjects.cpp
+++ classobjects.cpp
@@ -1,28 +1,28 @@
-// Program to calculate area and perimeter of rectangle using class-objects.
+// Program to calculate area and perimeter of triangle using class-objects.
 // Author: Inderpreet
 #include <iostream>
 using namespace std;
 class rectangle {
 	public:
-	float length,breadth,area,perimeter;
+	float base,height,area,perimeter;
 	void input(){
-		cout<<"Enter Length: ";
-		cin>>length;
-		cout<<"Enter Breadth: ";
-		cin>>breadth;
+		cout<<"Enter Base: ";
+		cin>>base;
+		cout<<"Enter Height: ";
+		cin>>height;
 	}
 	float findarea(){
-		area = length*breadth;
+		area = 0.5*base*height;
 		return area;
 	}
 	float findperimeter(){
-		perimeter = 2*(length+breadth);
+		perimeter = 3*base;
 		return perimeter;
 	}
 };
 int main() {
 	rectangle rect;
 	rect.input();
-	cout<<"Area of Rectangle = "<<rect.findarea();
-	cout<<"\nPerimeter of Rectangle = "<<rect.findperimeter();
+	cout<<"Area of Triangle = "<<rect.findarea();
+	cout<<"\nPerimeter of Triangle = "<<rect.findperimeter();
 }
